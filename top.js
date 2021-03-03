2c9e8b78016883194d633033aa0c468eb7a78390
EC2 Default User <ec2-user@ip-172-31-3-104.ap-northeast-1.compute.internal>
2021-03-03 06:44:52 +0000

    second commit

--- top.js
+++ top.js
@@ -12,4 +12,25 @@ $(function(){
         }, 500);
     });
     $('.about, .skills, .service, .work, .contact').fadeIn(2000);
+    
+    $(window).scroll(function (){
+    $('.about, .skills, .service, .work, .contact').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
+    
+   var $body = $('body');
+   $('#toggle').on('click', function(){
+  $body.addClass('is-menu-open');
+  return false;
+});
+$('.menu-close-btn').on('click', function(){
+  $body.removeClass('is-menu-open');
+  return false;
+});
 });
